5a78cc1e91f78f74b548ae85d24582fdcecef6f2
Danielle Higgins <higdan4024@gmail.com>
2025-03-13 23:17:55 -0400

    added example usage to docs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // want to use the static files in the public folder
 app.use(express.static("public"));
